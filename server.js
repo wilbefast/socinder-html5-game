c698d24681000f8fd1c67e35e53d3bf1fb47f128
William Dyce <wilbefast@gmail.com>
2016-03-26 16:17:22 +0100

    initial facebook test (passport)

--- server.js
+++ server.js
@@ -10,33 +10,52 @@
 // SERVER
 // ----------------------------------------------------------------------------
 
-var express = require('express'); 
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var LOCALHOST = true; // set this to false to use AWS
 
-// ------------------------------------
-// Socket.io
-// ------------------------------------
+var FACEBOOK_APP_ID;
+var FACEBOOK_APP_SECRET;
+var SERVER_URL;
 
-io.on('connection', function(socket){
-  socket.emit('connection');
-
-	console.log("New player joined !");
-
-  socket.on('say', function(msg) {
-  	console.log("say", msg);
-  	if(socket.other)
-  		socket.other.emit('say', msg);
-  });
-
-});
+if(LOCALHOST) {
+	FACEBOOK_APP_ID = 1719505808267776;
+	FACEBOOK_APP_SECRET = "40c7610ad84f80ce2df9ad237d609455";
+	SERVER_URL = "http://localhost:3000"
+}
+else {
+	FACEBOOK_APP_ID = 1719504328267924;
+	FACEBOOK_APP_SECRET = "5a30ef2234b6114fd48d0c7e10d5b98e";
+	SERVER_URL = "http://localhost:3000"
+}
 
 // ------------------------------------
 // Express
 // ------------------------------------
 
+var express = require('express'); 
+var app = express();
+var http = require('http').Server(app);
+
 app.use(express.static("client"));
+
+// ------------------------------------
+// Passport
+// ------------------------------------
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: SERVER_URL + "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+  	console.log("facebook lol");
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
 
 // ------------------------------------
 // HTTP
@@ -44,4 +63,12 @@ app.use(express.static("client"));
 
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on port 3000');
+});
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', function(req, res) {
+  res.send('hello world');
+  console.log("facebook lolz");
 });
